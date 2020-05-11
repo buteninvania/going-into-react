0e0a0222bc577419503c8142d91cbd67a38dddec
Ivan <buteninvania@gmail.com>
2020-05-11 11:55:22 +0300

    First stage

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className = 'header'>
+            <img alt="logo" src= 'https://farm5.staticflickr.com/4719/38474702340_7998619224_o.jpg'/>
+        </header>
+    );
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Content = () => {
+    return (
+        <div className = 'content'>
+            <div><img alt="CuberPunk" src='https://kartinkinaden.ru/uploads/posts/2019-08/1565900257_art-kiberpank-122.jpg' /></div>
+            <div>
+                <img alt="avatar" src='https://avavatar.ru/images/full/34/yn32JIbbMXNI6Fpd.jpg' />
+                description        
+            </div>
+        <div>
+            my post
+            <div>new post</div>
+            <div>post1</div>
+            <div>post2</div>
+        </div>
+      </div>
+    );
+}
+
+export default Content;
